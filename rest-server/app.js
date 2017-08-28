9919d80f79f2bc89b0eb9cbb068d146ebb96cc85
fivehao <fivehao0919@hotmail.com>
2017-08-29 00:12:46 +0800

    add

--- rest-server/app.js
+++ rest-server/app.js
@@ -16,7 +16,7 @@ var app = express();
 var mongoose = require('mongoose');
 
 var url = 'mongodb://localhost:27017/conFusion';
-mongoose.cnnect(url);
+mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
