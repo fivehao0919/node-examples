5cfa5836fd63fae092b4bcb911e74d11dd6567ac
wuhao4 <wuhao4@100tal.com>
2017-09-04 18:32:39 +0800

    add

---
+++ rest-server-passport/models/leadership.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leadershipSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        default: '',
+        required: true
+    },
+    abbr: {
+        type: String,
+        default: '',
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Leaders = mongoose.model('Leader', leadershipSchema);
+
+// make this available to our Node applications
+module.exports = Leaders;
