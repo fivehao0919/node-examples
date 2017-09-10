1c97adae749729baa926582a75029f2de70dcc5e
fivehao <fivehao0919@hotmail.com>
2017-09-10 22:23:36 +0800

    add

--- rest-server-passport/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientID: '141446143128785',
-        clientSecret: '32faf6f7092e71aaa66e863ed3955ca1',
-        callbackURL: "https//localhost:3443/users/facebook/callback"
-    }
-}
